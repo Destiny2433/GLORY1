80ddb09927ec03933d423d8d2f583158049f4db3
Destiny <okonudestiny4@gmail.com>
2026-09-17 21:45:36 +0100

    djd

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import (
     Flask, render_template, request, redirect, url_for, g,
-    session, flash, jsonify, send_from_directory, abort
+    session, flash, jsonify, send_from_directory, abort, Response
 )
 
 import os
